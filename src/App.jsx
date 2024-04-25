acb374269af70aa1c22801c9f83fc6331774e0fe
Will Baxter <bax.will@googlemail.com>
2024-04-25 15:16:20 +0100

    Split header and left nav into components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import initialEmails from './data/emails'
 
 import './styles/App.css'
 
-const getReadEmails = emails => emails.filter(email => !email.read)
+import Header from './Components/Header.jsx'
 
+import LeftNavBar from './Components/LeftNavBar.jsx'
+
+const getReadEmails = emails => emails.filter(email => !email.read)
 const getStarredEmails = emails => emails.filter(email => email.starred)
 
 function App() {
@@ -17,7 +20,7 @@ function App() {
   const starredEmails = emails.filter(email => email.starred)
 
   const toggleStar = targetEmail => {
-    const updatedEmails = emails =>
+    const updatedEmails = emails => 
       emails.map(email =>
         email.id === targetEmail.id
           ? { ...email, starred: !email.starred }
@@ -43,22 +46,9 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
+      <Header />
+      <LeftNavBar currentTab={currentTab} setCurrentTab={setCurrentTab} emails={emails} />
+      
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
